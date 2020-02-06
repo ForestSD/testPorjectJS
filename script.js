8fd460030a4465869bc56a85f36183d65a26ad6c
Forest_SD <artforest98@gmail.com>
2020-02-06 18:27:35 +0300

    тестовый пример

---
+++ script.js
@@ -0,0 +1,7 @@
+let ansver = confirm("Тебе 18 то есть?");
+let kek = "http://picscomment.com/uhadi-3.html";
+if(ansver == true){
+    alert("Взрослый...Ну окей, смотри свою порнушку");
+}else{
+    alert(kek);
+}
